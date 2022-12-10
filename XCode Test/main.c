a9a454d4c5360eca8351d5f333fb73a962045074
jjones9527 <jjones9527@gmail.com>
2022-12-09 21:00:59 -0500

    Initial Commit

---
+++ XCode Test/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  XCode Test
+//
+//  Created by Jeremy Jones on 2022-12-09.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
